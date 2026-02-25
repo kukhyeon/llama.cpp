01fcce1101a15b9de90d2c6de1249df5f0c0506b
kukhyeon <sha5686a@example.com>
2026-02-25 12:29:04 +0900

    Fix: Add RAM frequency info for S25

--- src/hard/device.cpp
+++ src/hard/device.cpp
@@ -1,37 +1,13 @@
 #include "device.h"
-#include <iostream>
-#include <algorithm>
 
-// trim from start (in place)
-static inline void ltrim(std::string &s) {
-    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
-        return !std::isspace(ch);
-    }));
-}
-
-// trim from end (in place)
-static inline void rtrim(std::string &s) {
-    s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
-        return !std::isspace(ch);
-    }).base(), s.end());
-}
 
 Device::Device(const std::string& device_name) : device(device_name){
-    // trim whitespace
-    ltrim(this->device);
-    rtrim(this->device);
-    
-    // std::cout << "[DEBUG] Device name: '" << this->device << "'" << std::endl;
-
-    if (this->device == "S22_Ultra" || this->device == "Fold4" ||  this->device == "Pixel9"){
+    if (device_name == "S22_Ultra" || device_name == "Fold4" ||  device_name == "Pixel9"){
 	    cluster_indices = {0, 4, 7};
-    } else if (this->device == "S24"){
+    } else if (device_name == "S24"){
 	    cluster_indices = {0, 4, 7, 9};
-    } else if (this->device == "S25"){
+    } else if (device_name == "S25"){
         cluster_indices = {0, 6};
-    } else {
-        std::cerr << "[WARNING] Unknown device name: " << this->device << std::endl;
-    }
 }
 
 const std::vector<int> Device::get_cluster_indices() const{
