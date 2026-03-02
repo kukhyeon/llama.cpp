73e7c566515fbee720034eae287c7ca9e52bed67
kukhyeon <ska5686a@gmail.com>
2026-03-02 14:42:05 +0900

    Update: gitignore

--- scripts-termux/build_termux.sh
+++
@@ -1,8 +0,0 @@
-#! bin/bash
-
-cmake -S . \
-	-DCMAKE_C_FLAGS="-march=armv8-a+dotprod+sve -pthread" \
-	-DCMAKE_CXX_FLAGS="-march=armv8-a+dotprod+sve -pthread" \
-	-B build-termux
-
-cmake --build build-termux --config Release -j4
