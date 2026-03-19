8bfa0ea2a160173af8707d82d9732ac38e3059d4
kukhyeon <kukhyeon@github.com>
2026-03-19 13:07:59 +0900

    Add: Output length fixed dataset

--- scripts-termux/run.sh
+++ scripts-termux/run.sh
@@ -1,7 +1,7 @@
 # this script should be run on llama.cpp/ dir.
 
 # screen brightness control
-su -c "echo 0 > /sys/class/backlight/panel0-backlight/brightness"
+echo 0 > /sys/class/backlight/panel0-backlight/brightness
 
 # silver core control
 # su -c "echo 1 > /sys/devices/system/cpu/cpu1/online"
@@ -9,15 +9,15 @@ su -c "echo 0 > /sys/class/backlight/panel0-backlight/brightness"
 # su -c "echo 1 > /sys/devices/system/cpu/cpu3/online"
 
 # CPU Governor: performance
-su -c "echo performance > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor"
-su -c "echo performance > /sys/devices/system/cpu/cpufreq/policy6/scaling_governor"
+echo performance > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
+echo performance > /sys/devices/system/cpu/cpufreq/policy6/scaling_governor
 echo "CPU Governor (policy0): $(cat /sys/devices/system/cpu/cpufreq/policy0/scaling_governor)"
 echo "CPU Governor (policy6): $(cat /sys/devices/system/cpu/cpufreq/policy6/scaling_governor)"
 sleep 3
 
 ./build/bin/ignite \
     -m models/qwen-1.5-1.8b-chat-q4_k_m.gguf \
-    -i -cnv -tb 5 -t 5 -i -ub 512 -b 512 \
+    -i -cnv -tb 5 -t 5 -ub 512 -b 512 \
     -c 1024 \
     --temp 0 \
     --top-k 1 \
@@ -39,11 +39,11 @@ sleep 3
 # su -c "echo 1 > /sys/devices/system/cpu/cpu3/online"
 
 # CPU Governor reset: walt
-su -c "echo walt > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor"
-su -c "echo walt > /sys/devices/system/cpu/cpufreq/policy6/scaling_governor"
+echo walt > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
+echo walt > /sys/devices/system/cpu/cpufreq/policy6/scaling_governor
 echo "CPU Governor reset (policy0): $(cat /sys/devices/system/cpu/cpufreq/policy0/scaling_governor)"
 echo "CPU Governor reset (policy6): $(cat /sys/devices/system/cpu/cpufreq/policy6/scaling_governor)"
 
 # experiment done -> let screen brightness bright again
-su -c "echo 1023 > /sys/class/backlight/panel0-backlight/brightness"
+echo 1023 > /sys/class/backlight/panel0-backlight/brightness
 
