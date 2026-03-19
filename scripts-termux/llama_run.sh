ae86a94886a0fb8e7146e26fb1adceede5b91d8e
kukhyeon <ska5686@gmail.com>
2026-03-19 20:07:10 +0900

    d

--- scripts-termux/llama_run.sh
+++ scripts-termux/llama_run.sh
@@ -23,7 +23,7 @@ sleep 3
     --top-k 1 \
     --device-name S25 \
     --output-dir output/ \
-    --input-path data/llama32_prefill_64.json \
+    --input-path data/llama32_prefill_256.json \
     -fa off \
     --strict on \
     --strict-limit 2 \
