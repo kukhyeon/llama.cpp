05bc9075c870038aea0b5d9765ec123b2cd0f54f
kukhyeon <ska5686@gmail.com>
2026-03-19 18:55:59 +0900

    Add: Prefill token test

--- scripts-termux/llama_run.sh
+++ scripts-termux/llama_run.sh
@@ -23,10 +23,10 @@ sleep 3
     --top-k 1 \
     --device-name S25 \
     --output-dir output/ \
-    --input-path data/64_qa_20.json \
+    --input-path data/query_test.json \
     -fa off \
     --strict on \
-    --strict-limit 256 \
+    --strict-limit 2 \
     --cpu-p 15 \
     --ram-p 9 \
     --cpu-d 15 \
